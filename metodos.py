3856cec0f46fad6cb8156fa282273ae84804e2a5
Jose Blanco ULE <jblanf05@estudiantes.unileon.es>
2025-04-23 16:15:47 +0200

    Version1

---
+++ metodos.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+
+class Metodos:
+
+    @staticmethod
+    def cargar_keywords_desde_txt(ruta_archivo):
+        try:
+            with open(ruta_archivo, "r", encoding="utf-8") as f:
+                lineas = f.readlines()
+            keywords = [linea.strip() for linea in lineas if linea.strip()]
+            print(f"🔤 {len(keywords)} palabras clave cargadas desde '{ruta_archivo}'")
+            return keywords
+        except Exception as e:
+            print(f"❌ Error al leer {ruta_archivo}: {e}")
+            return []
+
+    @staticmethod
+    def save_resultados_by_year(diccionario, carpeta, nombre):
+        os.makedirs(carpeta, exist_ok=True)
+
+        for year, items in diccionario.items():
+
+            print(f"[Guardar] {len(items)} resultados en {nombre}_{year}")
+
+            with open(f"{carpeta}/{nombre}_{year}.txt", "w", encoding="utf-8") as f:
+                for i, item in enumerate(items, 1):
+                    f.write(f"{i}. {item['Título']}\n")
+                    f.write(f"Fecha: {item['Fecha']}\n")
+                    f.write(f"Fuente: {item['Fuente']}\n")
+                    f.write(f"URL: {item['URL']}\n")
+                    f.write(f"{item['Descripción']}\n\n")
+
+    @staticmethod
+    def print_resume(dataset, name):
+        print(f"\n📊 Conteo final de {name}:")
+
+        print("\n" + "=" * 40)
+        print(f"{name}")
+        print("=" * 40)
+
+        for year in sorted(dataset.keys()):
+            print(f" - {year}: {len(dataset[year])} resultados")
+
+        print(f"\n✅ Conteo de {name} completado con éxito.")
